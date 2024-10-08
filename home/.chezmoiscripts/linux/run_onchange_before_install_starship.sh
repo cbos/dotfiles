71ace87f8a34448cf40783f7a0d9772edda0b95d
Cees Bos <dev@ceesbos.nl>
2024-10-08 10:22:54 +0200

    Fix problem with installation of Starship, force installation without interaction

--- home/.chezmoiscripts/linux/run_onchange_before_install_starship.sh
+++ home/.chezmoiscripts/linux/run_onchange_before_install_starship.sh
@@ -4,6 +4,6 @@ set -e # -e: exit on error
 
 if [ ! "$(command -v starship)" ]; then
   # Download and install starship. Add the --force argument, otherwise it cannot be executed without manual confirmation
-  curl -sS https://starship.rs/install.sh | xargs -I {} sh -c {} -s --force
+  curl -sS https://starship.rs/install.sh | sh -s -- --yes
 fi
 
