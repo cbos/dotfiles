e7e469028893bd6dbd67affbba4e0e014ab15841
Cees Bos <dev@ceesbos.nl>
2025-07-01 13:41:26 +0200

    Remove starship download

--- home/.chezmoiscripts/linux/run_onchange_before_install_mc.sh
+++ home/.chezmoiscripts/linux/run_onchange_before_install_mc.sh
@@ -5,9 +5,6 @@ set -e # -e: exit on error
 mkdir -p $HOME/.local/bin/
 
 if [ ! "$(command -v mc)" ]; then
-  # Download and install starship. Add the --force argument, otherwise it cannot be executed without manual confirmation
-  curl -sS https://starship.rs/install.sh | sh -s -- --yes
-
   curl https://dl.min.io/client/mc/release/linux-amd64/mc \
     --create-dirs \
     -o $HOME/.local/bin/mc
