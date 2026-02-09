b138b41be6e94feef757592866c703e513a18ea4
Cees Bos <dev@ceesbos.nl>
2026-02-09 14:35:32 +0100

    Update Eza installation script

--- home/.chezmoiscripts/linux/run_onchange_before_install_eza.sh
+++ home/.chezmoiscripts/linux/run_onchange_before_install_eza.sh
@@ -9,6 +9,10 @@ if [ ! "$(command -v eza)" ]; then
   else
       if [ "$(command -v brew)" ]; then
         brew install eza
+      else
+        wget -c https://github.com/eza-community/eza/releases/latest/download/eza_x86_64-unknown-linux-gnu.tar.gz -O - | tar xz
+        chmod +x eza
+        mv eza $HOME/.local/bin/eza
       fi
   fi
 fi
